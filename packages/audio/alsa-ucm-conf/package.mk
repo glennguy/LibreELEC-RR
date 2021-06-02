c6f0066465bc0cf48adc85b22df9d595bd7ac2e1
heitbaum <rudi@heitbaum.com>
2021-06-02 10:06:24 +0000

    alsa-ucm-conf: update to 1.2.5.1

--- packages/audio/alsa-ucm-conf/package.mk
+++ packages/audio/alsa-ucm-conf/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="alsa-ucm-conf"
-PKG_VERSION="1.2.4"
-PKG_SHA256="2c3b535c77dcb9aaf62a61f4f8324f1ab184162f105f7ec9ed1e37c742fcd340"
+PKG_VERSION="1.2.5.1"
+PKG_SHA256="5841a444166dcbf479db751303dbc3556f685085ac7e00f0c9e7755676195d97"
 PKG_LICENSE="BSD-3c"
 PKG_SITE="http://www.alsa-project.org/"
 PKG_URL="ftp://ftp.alsa-project.org/pub/lib/alsa-ucm-conf-${PKG_VERSION}.tar.bz2"
