808efc68db0f9dd22aedde972c617e8ad124b6a5
heitbaum <rudi@heitbaum.com>
2021-07-08 09:49:19 +0000

    libuv: update to 1.41.1

--- packages/addons/addon-depends/ttyd-depends/libuv/package.mk
+++ packages/addons/addon-depends/ttyd-depends/libuv/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libuv"
-PKG_VERSION="1.41.0"
-PKG_SHA256="6cfeb5f4bab271462b4a2cc77d4ecec847fdbdc26b72019c27ae21509e6f94fa"
+PKG_VERSION="1.41.1"
+PKG_SHA256="62c29d1d76b0478dc8aaed0ed1f874324f6cd2d6ff4cb59a44026c09e818cd53"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libuv/libuv"
 PKG_URL="https://github.com/libuv/libuv/archive/v${PKG_VERSION}.tar.gz"
