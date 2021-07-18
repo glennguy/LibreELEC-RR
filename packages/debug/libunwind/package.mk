bf6573a4e2506cc71cd94b70ba166f26964dced1
heitbaum <rudi@heitbaum.com>
2021-07-18 08:20:39 +0000

    libunwind: update to 1.5.0

--- packages/debug/libunwind/package.mk
+++ packages/debug/libunwind/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libunwind"
-PKG_VERSION="1.4.0"
-PKG_SHA256="df59c931bd4d7ebfd83ee481c943edf015138089b8e50abed8d9c57ba9338435"
+PKG_VERSION="1.5.0"
+PKG_SHA256="90337653d92d4a13de590781371c604f9031cdb50520366aa1e3a91e1efb1017"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.nongnu.org/libunwind/"
 PKG_URL="http://download.savannah.nongnu.org/releases/libunwind/libunwind-${PKG_VERSION}.tar.gz"
