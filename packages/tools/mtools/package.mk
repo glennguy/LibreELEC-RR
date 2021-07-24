0ef2408cf018adeb590b5eee7ce0420c2610611f
heitbaum <rudi@heitbaum.com>
2021-07-24 10:07:12 +0000

    Revert "mtools: update to 4.0.34"
    This reverts commit 4286fd65463f788f8a74282ed2175ac858d91ed2.

--- packages/tools/mtools/package.mk
+++ packages/tools/mtools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mtools"
-PKG_VERSION="4.0.34"
-PKG_SHA256="ada543eb1b0ffe5440d4ee42634309ea1e286d7ebb80c99ba50db5cb2c360cab"
+PKG_VERSION="4.0.29"
+PKG_SHA256="c6b50b4603e7afe54ba0109094669373028db9af743f5c541bca4b7664aefec7"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/mtools/"
 PKG_URL="http://ftpmirror.gnu.org/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
