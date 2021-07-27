61754227f001a10d7f96181e69489a1798586296
heitbaum <rudi@heitbaum.com>
2021-07-27 22:29:49 +1000

    u-boot: update to 2021.10-rc1

--- packages/tools/u-boot/package.mk
+++ packages/tools/u-boot/package.mk
@@ -23,15 +23,15 @@ PKG_NEED_UNPACK="${PROJECT_DIR}/${PROJECT}/bootloader"
 
 case "${PROJECT}" in
   Amlogic)
-    PKG_VERSION="2021.07"
-    PKG_SHA256="312b7eeae44581d1362c3a3f02c28d806647756c82ba8c72241c7cdbe68ba77e"
+    PKG_VERSION="2021.10-rc1"
+    PKG_SHA256="19fedf084b709eda6fa88104ae9d7a9b5c0e75e828a98406238f085109c2597b"
     #PKG_URL="https://github.com/chewitt/u-boot/archive/${PKG_VERSION}.tar.gz"
     PKG_URL="http://ftp.denx.de/pub/u-boot/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
     PKG_PATCH_DIRS="amlogic"
     ;;
   Rockchip)
-    PKG_VERSION="2021.07"
-    PKG_SHA256="312b7eeae44581d1362c3a3f02c28d806647756c82ba8c72241c7cdbe68ba77e"
+    PKG_VERSION="2021.10-rc1"
+    PKG_SHA256="19fedf084b709eda6fa88104ae9d7a9b5c0e75e828a98406238f085109c2597b"
     PKG_URL="http://ftp.denx.de/pub/u-boot/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
     #PKG_VERSION="8659d08d2b589693d121c1298484e861b7dafc4f"
     #PKG_SHA256="3f9f2bbd0c28be6d7d6eb909823fee5728da023aca0ce37aef3c8f67d1179ec1"
@@ -39,8 +39,8 @@ case "${PROJECT}" in
     PKG_PATCH_DIRS="rockchip"
     ;;
   *)
-    PKG_VERSION="2021.07"
-    PKG_SHA256="312b7eeae44581d1362c3a3f02c28d806647756c82ba8c72241c7cdbe68ba77e"
+    PKG_VERSION="2021.10-rc1"
+    PKG_SHA256="19fedf084b709eda6fa88104ae9d7a9b5c0e75e828a98406238f085109c2597b"
     PKG_URL="http://ftp.denx.de/pub/u-boot/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
     PKG_PATCH_DIRS="default"
     ;;
