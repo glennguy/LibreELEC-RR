7de50146b3910dd54f990226db13359410f132db
heitbaum <rudi@heitbaum.com>
2021-07-27 22:30:22 +1000

    automake: update to 1.16.4

--- packages/devel/automake/package.mk
+++ packages/devel/automake/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="automake"
-PKG_VERSION="1.16.3"
-PKG_SHA256="ff2bf7656c4d1c6fdda3b8bebb21f09153a736bcba169aaf65eab25fa113bf3a"
+PKG_VERSION="1.16.4"
+PKG_SHA256="80facc09885a57e6d49d06972c0ae1089c5fa8f4d4c7cfe5baea58e5085f136d"
 PKG_LICENSE="GPL"
 PKG_SITE="http://sources.redhat.com/automake/"
 PKG_URL="http://ftpmirror.gnu.org/automake/${PKG_NAME}-${PKG_VERSION}.tar.xz"
