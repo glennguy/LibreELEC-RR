71eee5557d070421e7fcfa045ec3fe50a1796165
SupervisedThinking <supervisedthinking@gmail.com>
2021-07-28 22:15:08 +0200

    gambatte: updated to git ff1ae4a

--- packages/supervisedthinking/emulation/libretro/gambatte/package.mk
+++ packages/supervisedthinking/emulation/libretro/gambatte/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="gambatte"
-PKG_VERSION="43cd6ec5b7bca1ece3eee45387393374c17afaad"
-PKG_SHA256="cb55e7ad63ad72f88eaf546c621bc35410c5c1b7828db0985e0041ddaf38dbe4"
+PKG_VERSION="ff1ae4afe2cdf4afbf51e36a236f2f70a966dd26"
+PKG_SHA256="777adf222dc287ad07f7348a929e0bd0c23a0010f448367732fd9acc8592bb61"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/gambatte-libretro"
 PKG_URL="https://github.com/libretro/gambatte-libretro/archive/${PKG_VERSION}.tar.gz"
