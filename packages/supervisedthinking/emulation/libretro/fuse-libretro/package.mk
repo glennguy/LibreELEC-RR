d88b20e0532193bd14a8b67c54744745010f4cde
SupervisedThinking <supervisedthinking@gmail.com>
2021-07-28 22:14:52 +0200

    fuse-libretro: updated to git 5b1c053

--- packages/supervisedthinking/emulation/libretro/fuse-libretro/package.mk
+++ packages/supervisedthinking/emulation/libretro/fuse-libretro/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="fuse-libretro"
-PKG_VERSION="e52a9eb4d094a1afc2af00fa7af51d891a4232a8"
-PKG_SHA256="8ef9282b9c53ae02108c5fd1f955200fb184c99d4608bfc9c6cc5823ffd4ddbe"
+PKG_VERSION="5b1c05330e907556d99a8caca769ce273fdc9198"
+PKG_SHA256="97872e72a6bba6f13e0db5f9b94586699f11a68418a446398dc94cf6cf0d62ac"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/libretro/fuse-libretro"
 PKG_URL="https://github.com/libretro/fuse-libretro/archive/${PKG_VERSION}.tar.gz"
