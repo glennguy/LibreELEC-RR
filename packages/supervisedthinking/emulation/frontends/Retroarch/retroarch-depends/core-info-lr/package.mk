6894a2e0b8f67f0fd9fb6de6e0b92f1af8e85a1b
SupervisedThinking <supervisedthinking@gmail.com>
2021-07-28 21:44:39 +0200

    core-info-lr: updated to 1.9.6

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/core-info-lr/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/core-info-lr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="core-info-lr"
-PKG_VERSION="1.9.3"
-PKG_SHA256="70d97e53604a128d67c2b0cb08e2ae9339f741e72d7e0211621e4ce8e9b4549d"
+PKG_VERSION="1.9.6"
+PKG_SHA256="68fe43be62232b797dfd4e7fdc1993bb836c358509be4a070863b4a6a617e4de"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/libretro-core-info"
 PKG_URL="https://github.com/libretro/libretro-core-info/archive/refs/tags/v${PKG_VERSION}.tar.gz"
