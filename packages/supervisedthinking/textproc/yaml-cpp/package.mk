593a5247bb94fa10aaec56b0f7cf9736a1f5bf88
SupervisedThinking <supervisedthinking@gmail.com>
2021-07-28 21:47:35 +0200

    yaml-cpp: updated to 0.7.0

--- packages/supervisedthinking/textproc/yaml-cpp/package.mk
+++ packages/supervisedthinking/textproc/yaml-cpp/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="yaml-cpp"
-PKG_VERSION="a6bbe0e50ac4074f0b9b44188c28cf00caf1a723" #v0.6.3+
-PKG_SHA256="e34592214d738498f2d2b4080c607dc59e5e63602b4b7af8d89bcfcc3f390015"
+PKG_VERSION="0.7.0"
+PKG_SHA256="43e6a9fcb146ad871515f0d0873947e5d497a1c9c60c58cb102a97b47208b7c3"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/jbeder/yaml-cpp"
-PKG_URL="https://github.com/jbeder/yaml-cpp/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/jbeder/yaml-cpp/archive/refs/tags/yaml-cpp-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain glibc gcc"
 PKG_LONGDESC="yaml-cpp is a YAML parser and emitter in C++ matching the YAML 1.2 spec."
 
