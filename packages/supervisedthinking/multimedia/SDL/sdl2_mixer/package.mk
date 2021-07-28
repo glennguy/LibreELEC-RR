0e18807318bd268a22d1eb56f014a1f558522015
SupervisedThinking <supervisedthinking@gmail.com>
2021-07-28 21:49:42 +0200

    sdl2_mixer: updated to git fe90300

--- packages/supervisedthinking/multimedia/SDL/sdl2_mixer/package.mk
+++ packages/supervisedthinking/multimedia/SDL/sdl2_mixer/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="sdl2_mixer"
-PKG_VERSION="228ecedc257a24e384e4af35899190541000fb07"
-PKG_SHA256="8988afbf15b7c190d6e8a2a08bfa153cfb16440abbdbb38fbc7f8d3f25c0cb17"
+PKG_VERSION="fe903003892c00ed4a6641a5c8c2cc09057e065d"
+PKG_SHA256="efd785e94f80e1da555d184d70c87b017104147c25f666488b561b6adae27971"
 PKG_LICENSE="SDL"
 PKG_SITE="https://libsdl.org/projects/SDL_mixer/"
 PKG_URL="https://github.com/libsdl-org/SDL_mixer/archive/${PKG_VERSION}.tar.gz"
