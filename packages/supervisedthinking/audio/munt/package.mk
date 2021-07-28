0d211afbfdaac7ca81165cd5f8acc26c7f4e2feb
SupervisedThinking <supervisedthinking@gmail.com>
2021-07-28 21:40:57 +0200

    munt: update to 2.5.2 / updated pkg url

--- packages/supervisedthinking/audio/munt/package.mk
+++ packages/supervisedthinking/audio/munt/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="munt"
-PKG_VERSION="1f42d80d9a4f1b0d5a499b783b9c5808c24c1a3e" # libmt32emu 2.5.0
-PKG_SHA256="80a577d985d21c6bb247e79c3342da3760527ae4f6f46fcb8809e2e7e96d4561"
+PKG_VERSION="2.5.2"
+PKG_SHA256="4ec903cc2bb2e6f1006663941a86949302147c990b245d6a6c41e8d3bc358b83"
 PKG_LICENSE="LGPL-2.0-or-later"
 PKG_SITE="https://github.com/munt/munt"
-PKG_URL="https://github.com/munt/munt/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/munt/munt/archive/refs/tags/libmt32emu_${PKG_VERSION//./_}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A software synthesiser emulating pre-GM MIDI devices such as the Roland MT-32."
 
