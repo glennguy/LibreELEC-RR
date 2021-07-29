0784828f1bea72a4db1979d4ac72e0c2917f8e4d
heitbaum <rudi@heitbaum.com>
2021-07-29 21:46:27 +1000

    jasper: update to 2.0.32

--- packages/addons/addon-depends/jasper/package.mk
+++ packages/addons/addon-depends/jasper/package.mk
@@ -1,18 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="jasper"
-PKG_VERSION="1.900.1"
-PKG_SHA256="6e9a959bf4f8cb02f77f42d1b9880b8e85d021ac51f43d8787b5438fd2b7a1c5"
+PKG_VERSION="2.0.32"
+PKG_SHA256="a3583a06698a6d6106f2fc413aa42d65d86bedf9a988d60e5cfa38bf72bc64b9"
 PKG_LICENSE="OpenSource"
 PKG_SITE="http://www.ece.uvic.ca/~mdadams/jasper/"
-PKG_URL="${DISTRO_SRC}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://github.com/jasper-software/jasper/archive/refs/tags/version-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libjpeg-turbo"
 PKG_LONGDESC="A implementation of the ISO/IEC 15444-1 also known as JPEG-2000 standard for image compression."
 PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
-
-post_makeinstall_target() {
-  rm -rf ${INSTALL}/usr/bin
-}
+PKG_CMAKE_OPTS_TARGET="-DJAS_ENABLE_DOC=false \
+                       -DJAS_ENABLE_PROGRAMS=false \
+                       -DJAS_ENABLE_SHARED=false"
