e46d43f37b947e649c6bb4708e8ee5e2ebdbb44d
heitbaum <rudi@heitbaum.com>
2021-08-01 19:41:28 +1000

    strace: is not supported on riscv64
    review this:
    - https://github.com/riscvarchive/riscv-strace

--- packages/debug/strace/package.mk
+++ packages/debug/strace/package.mk
@@ -4,6 +4,7 @@
 PKG_NAME="strace"
 PKG_VERSION="5.13"
 PKG_SHA256="5acc34888b9d510ad6ac915d4a8df08f51cf1ae920ea24649f6a4bb984d0b656"
+PKG_ARCH="arm aarch64 x86_64"
 PKG_LICENSE="BSD"
 PKG_SITE="https://strace.io/"
 PKG_URL="https://strace.io/files/${PKG_VERSION}/strace-${PKG_VERSION}.tar.xz"
