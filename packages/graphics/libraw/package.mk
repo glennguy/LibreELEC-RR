849045a7e1fd6c40b83bbbe63f3b95b5b4b82848
heitbaum <rudi@heitbaum.com>
2021-08-02 19:28:16 +1000

    libraw: fix hardcoded automake 1.15 version

--- packages/graphics/libraw/package.mk
+++ packages/graphics/libraw/package.mk
@@ -19,3 +19,8 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --disable-jasper \
                            --enable-lcms \
                            --disable-examples"
+
+pre_configure_target() {
+  cd ..
+  autoreconf -f
+}
