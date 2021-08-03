a26e634700259984939144e6c47a2058e4a2d9e8
heitbaum <rudi@heitbaum.com>
2021-08-04 01:13:10 +1000

    stress-ng: update to 0.13.00

--- packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
+++ packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="stress-ng"
-PKG_VERSION="0.12.12"
-PKG_SHA256="f27af50f6f2308e707fef927674bdd209a046b116734281b792aeca35a4e4499"
+PKG_VERSION="0.13.00"
+PKG_SHA256="1cefe4a3057c1522b146e62f61b80ce6e2e99da2d85ebe25bc03fc45228e58cd"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://kernel.ubuntu.com/~cking/stress-ng/"
 PKG_URL="https://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-${PKG_VERSION}.tar.xz"
