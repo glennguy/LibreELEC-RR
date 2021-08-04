8e4d94bbdd24f712cc3e496b28e0edaae11c2f38
SupervisedThinking <supervisedthinking@gmail.com>
2021-08-04 23:28:50 +0200

    pcsx2: updated to git 21c13a8 / dropped gsdx lib path

--- packages/supervisedthinking/emulation/standalone/pcsx2/package.mk
+++ packages/supervisedthinking/emulation/standalone/pcsx2/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="pcsx2"
-PKG_VERSION="85d35311d0b8048c8ced4597dafb2c1797918599" #v1.7.0-dev
+PKG_VERSION="21c13a850caf25c3ae0fecb54b9640099c20cfc8" #v1.7.0-dev
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/PCSX2/pcsx2"
