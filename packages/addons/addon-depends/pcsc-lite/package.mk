f6269376f0da738950f1acdace054e6334ac388f
heitbaum <rudi@heitbaum.com>
2021-08-07 15:16:40 +1000

    pcsc-lite: update to 1.9.3

--- packages/addons/addon-depends/pcsc-lite/package.mk
+++ packages/addons/addon-depends/pcsc-lite/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pcsc-lite"
-PKG_VERSION="1.9.2"
-PKG_SHA256="b267098e6d822903fba23b6183c17fd3678018b8544d1510bb7f2be802906b42"
+PKG_VERSION="1.9.3"
+PKG_SHA256="6956433c71dd17a902cd3f4a394ce48a1ea787faed526faf557c95cc434d3e59"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pcsclite.apdu.fr"
 PKG_URL="https://pcsclite.apdu.fr/files/pcsc-lite-${PKG_VERSION}.tar.bz2"
