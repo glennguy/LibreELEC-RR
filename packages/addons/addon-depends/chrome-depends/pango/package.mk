0d2d8de3526c8259cd2da8474c3d236c1194d37b
heitbaum <rudi@heitbaum.com>
2021-08-12 20:35:56 +1000

    pango: update to 1.48.8

--- packages/addons/addon-depends/chrome-depends/pango/package.mk
+++ packages/addons/addon-depends/chrome-depends/pango/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pango"
-PKG_VERSION="1.48.7"
-PKG_SHA256="28a82f6a6cab60aa3b75a90f04197ead2d311fa8fe8b7bfdf8666e2781d506dc"
+PKG_VERSION="1.48.8"
+PKG_SHA256="009d9d830ecbe11911d6637e48eec1c51390d3d12eb286035ef7c641f3c87410"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.pango.org/"
 PKG_URL="https://ftp.gnome.org/pub/gnome/sources/pango/${PKG_VERSION:0:4}/pango-${PKG_VERSION}.tar.xz"
