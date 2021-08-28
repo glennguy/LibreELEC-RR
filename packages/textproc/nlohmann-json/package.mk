269841770f090c7547d93a59aca7f53e15905d66
heitbaum <rudi@heitbaum.com>
2021-08-28 14:02:35 +1000

    nlohmann-json: update to 3.10.2

--- packages/textproc/nlohmann-json/package.mk
+++ packages/textproc/nlohmann-json/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nlohmann-json"
-PKG_VERSION="3.10.1"
-PKG_SHA256="c35d8c867f7cde3f6100c3f5dbe999667f77f075cd802bbd37080bd63ceb94c4"
+PKG_VERSION="3.10.2"
+PKG_SHA256="081ed0f9f89805c2d96335c3acfa993b39a0a5b4b4cef7edb68dd2210a13458c"
 PKG_LICENSE="MIT"
 PKG_SITE="https://nlohmann.github.io/json/"
 PKG_URL="https://github.com/nlohmann/json/archive/v${PKG_VERSION}.tar.gz"
